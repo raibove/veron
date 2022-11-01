a52fd074f0d72efa10ddc0020a12c4a35db50fb1
Shweta Kale <shwetakale144@gmail.com>
2022-11-01 16:36:14 +0530

    Setup react repository

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="bg-[#F4FFFE] h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
