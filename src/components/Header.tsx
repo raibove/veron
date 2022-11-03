cbb442de59563b582afb2b73d4318bafd8795c88
Shweta Kale <shwetakale144@gmail.com>
2022-11-03 11:43:23 +0530

    Add basic login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,36 @@
 import User from "../assets/user.svg";
 import Cart from "../assets/cart.svg";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { useState } from "react";
+import axios, { AxiosResponse } from "axios";
+
+interface AuthResponse {
+  token: string;
+  user: User;
+}
+
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
 
 const Header = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const handleLogin = async (googleData: any) => {
+    console.log(googleData);
+    try {
+      const result: AxiosResponse<AuthResponse> = await axios.post("/auth/", {
+        token: googleData?.credential,
+      });
+
+      setUser(result.data.user);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="h-20 bg-white shadow-lg flex items-center w-screen justify-between">
       <h1 className="text-[#008F83] font-bold text-4xl font-mono ml-4 cursor-default">
@@ -12,6 +41,16 @@ const Header = () => {
           <img src={User} alt="user" className="cursor-pointer" />
         </div>
         <img src={Cart} alt="cart" className="cursor-pointer" />
+        <GoogleOAuthProvider
+          clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}
+        >
+          <GoogleLogin
+            onSuccess={handleLogin}
+            onError={() => {
+              console.log("failed");
+            }}
+          />
+        </GoogleOAuthProvider>
       </div>
     </div>
   );
